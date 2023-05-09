b2c8f80738d682e82f7238cabd3dcc355b05a327
joseJoaoZavala <jose.zavala.set@gmail.com>
2023-05-09 10:44:48 +0200

    3

--- Apps/views/sistema.php
+++ Apps/views/sistema.php
@@ -1,38 +1,25 @@
 <?php
- 
-        include_once('config.php');
+    include_once('config.php');
 
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $nome = $_POST['nome'];
-        $email = $_POST['email'];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $username = isset($_POST['username']) ? $_POST['username'] : '';
+        $password = isset($_POST['password']) ? $_POST['password'] : '';
+        $nome = isset($_POST['nome']) ? $_POST['nome'] : '';
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
 
-        // if (empty($_POST['username']) || empty($_POST['password']) empty($_POST['nome']) empty($_POST['email'])) {
-        //     echo "<script>alert('Por favor prencha todos campos')</script>";
-        //     return false;
-        // }
-
-        $result = mysqli_query($conexao, ' INSERT INTO users(username, password, nome, email) VALUES ($username, $password, $nome, $email)');
-        
-        if (isset($_POST['username'])) {
-            $username = $_POST['username'];
-        $password = $_POST['password'];
-        $nome = $_POST['nome'];
-        $email = $_POST['email'];
-            
-
+        if (empty($username) || empty($password) || empty($nome) || empty($email)) {
+            echo "<script>alert('Por favor preencha todos campos')</script>";
+            return false;
         }
 
-    // $stmt->bindParam(':username', $username);
-    // $stmt->bindParam(':password', $passorword);
-    // $stmt->bindParam(':nome', $nome);
-    // $stmt->bindParam(':email', $email);
-    
-    // if($stmt->execute()){
-    //     echo "Dados inseridos com sucesso.";
-    // }else {
-    //     echo "erro ao inserir dados";
-    // }
+        $result = mysqli_query($conexao, "INSERT INTO users (id, username, password, nome, email) VALUES (null, '$username', '$password', '$nome', '$email')");
+
+        if ($result) {
+            echo "Dados inseridos com sucesso.";
+        } else {
+            echo "Erro ao inserir dados: " . mysqli_error($conexao);
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -42,47 +29,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
 </head>
 <body>
-    <form action="sistema.html" method="POST">
-        
-
+    <form action="sistema.php" method="POST">
         <div class="box">
             <fieldset>
-                <div class="inputBox">
 
+                <legend>Dados do Usuario</legend>
+                <div class="inputBox">
                     <label for="username">Username</label>
                     <input type="text" id="username" name="username">
-              
                 </div>
-
+<br>
                 <div class="inputBox">
-
                     <label for="password">Password</label>
-        <input type="password" id="password" name="password">
-
+                    <input type="password" id="password" name="password">
                 </div>
-
+<br>
                 <div class="inputBox">
-
                     <label for="nome">Nome</label>
-        <input type="text" id="nome" name="nome">
-                    
+                    <input type="text" id="nome" name="nome">
                 </div>
-
+<br>
                 <div class="inputBox">
                     <label for="email">Email</label>
                     <input type="text" id="email" name="email">
-                    
                 </div>
+<br>
 
                 <div class="inputBox">
                     <input type="submit" value="Enviar">
-                    
                 </div>
             </fieldset>
         </div>
-
     </form>
 </body>
 </html>
