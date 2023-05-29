d9086d9d58cc1e656a340ebbc259b1573d309553
MR ROBOT <judasmarcol@gmail.com>
2023-05-29 13:32:38 +0200

    reajuste  do tamanho das letras

--- Apps/views/index.php
+++ Apps/views/index.php
@@ -49,10 +49,9 @@
   <meta name="theme-color" content="#ffffff">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ 
   <!-- Vendors styles-->
   <link rel="stylesheet" href="../../assets/vendors/simplebar/css/simplebar.css">
   <link rel="stylesheet" href="../../assets/css/vendors/simplebar.css">
