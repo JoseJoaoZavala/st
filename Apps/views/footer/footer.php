8c6470d34f4a4b3bcd5d0ffe7060112ae86d3e75
joseJoaoZavala <jose.zavala.set@gmail.com>
2023-05-29 12:14:29 +0200

    9

--- Apps/views/footer/footer.php
+++ Apps/views/footer/footer.php
@@ -1,7 +1,6 @@
 
 <footer class="footer">
         <div><a href="https://set.co.mz">Sociedade & Territorio </a><a href="https://coreui.io"></a> © 2023.</div>
-        
       </footer>
     </div>
 
